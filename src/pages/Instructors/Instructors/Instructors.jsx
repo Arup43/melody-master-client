f4a1dc916f3490c92613041ee516ea60ed43fc68
Arup Debnath <1805043@ugrad.cse.buet.ac.bd>
2023-06-13 20:07:25 +0600

    Instructors page done

--- src/pages/Instructors/Instructors/Instructors.jsx
+++ src/pages/Instructors/Instructors/Instructors.jsx
@@ -1,44 +1,31 @@
+import { useEffect, useState } from "react";
 import Instructor from "../Instructor/Instructor";
 
 const Instructors = () => {
-    return (
-        <div className="pt-52 text-center max-w-screen-xl mx-auto mb-20">
+  const [instructors, setInstructors] = useState([]);
+  useEffect(() => {
+    fetch("http://localhost:5000/instructors")
+      .then((res) => res.json())
+      .then((data) => setInstructors(data));
+  }, []);
+  return (
+    <div className="pt-52 text-center max-w-screen-xl mx-auto mb-20">
       <h1 className="text-4xl">Visit Our All Teachers</h1>
-      <p className="mb-10">Choose with whom you want to learn your dream instrument.</p>
+      <p className="mb-10">
+        Choose with whom you want to learn your dream instrument.
+      </p>
       <div className="grid md:grid-cols-3 gap-10">
-        <Instructor
-          image="https://www.superprof.ca/blog/wp-content/uploads/2017/07/guitar-guitar-player-musician-instrument-guitarist.jpg"
-          name="Kuddus Alam"
-          email= "kuddos@alam.com"
-        ></Instructor>
-        <Instructor
-          image="https://www.superprof.ca/blog/wp-content/uploads/2017/07/guitar-guitar-player-musician-instrument-guitarist.jpg"
-          name="Kuddus Alam"
-          email= "kuddos@alam.com"
-        ></Instructor>
-        <Instructor
-          image="https://www.superprof.ca/blog/wp-content/uploads/2017/07/guitar-guitar-player-musician-instrument-guitarist.jpg"
-          name="Kuddus Alam"
-          email= "kuddos@alam.com"
-        ></Instructor>
-        <Instructor
-          image="https://www.superprof.ca/blog/wp-content/uploads/2017/07/guitar-guitar-player-musician-instrument-guitarist.jpg"
-          name="Kuddus Alam"
-          email= "kuddos@alam.com"
-        ></Instructor>
-        <Instructor
-          image="https://www.superprof.ca/blog/wp-content/uploads/2017/07/guitar-guitar-player-musician-instrument-guitarist.jpg"
-          name="Kuddus Alam"
-          email= "kuddos@alam.com"
-        ></Instructor>
-        <Instructor
-          image="https://www.superprof.ca/blog/wp-content/uploads/2017/07/guitar-guitar-player-musician-instrument-guitarist.jpg"
-          name="Kuddus Alam"
-          email= "kuddos@alam.com"
-        ></Instructor>
+        {
+          instructors.map(instructor => <Instructor
+            key={instructor._id}
+            image={instructor.imageURL}
+            name={instructor.name}
+            email={instructor.email}
+          ></Instructor>)
+        }
       </div>
     </div>
-    );
+  );
 };
 
 export default Instructors;
