252c7b9b4f4e782bfdb6b6f9eb7825d9092e1f33
Arup Debnath <1805043@ugrad.cse.buet.ac.bd>
2023-06-14 03:43:21 +0600

    Enrolled classes page done

--- src/pages/Dashboard/EnrolledClasses/EnrolledClasses.jsx
+++ src/pages/Dashboard/EnrolledClasses/EnrolledClasses.jsx
@@ -1,11 +1,51 @@
-
+import useEnrolledClasses from "../../../hooks/useEnrolledClasses";
 
 const EnrolledClasses = () => {
-    return (
-        <div>
-            <h1>This is my enrolled classes page</h1>
-        </div>
-    );
+  const [enrolledClasses] = useEnrolledClasses();
+  return (
+    <div>
+      <h1 className="text-center text-4xl font-bold mb-10">
+        My Enrolled Classes
+      </h1>
+      <div className="overflow-x-auto">
+        <table className="table">
+          {/* head */}
+          <thead>
+            <tr>
+              <th>Class Image</th>
+              <th>Class Name</th>
+              <th>Instructor Name</th>
+              <th>Price</th>
+            </tr>
+          </thead>
+          <tbody>
+            {/* row 1 */}
+            {enrolledClasses?.map((c) => {
+              return (
+                <tr key={c._id}>
+                  <td>
+                    <div className="flex items-center space-x-3">
+                      <div className="avatar">
+                        <div className="mask mask-squircle w-12 h-12">
+                          <img
+                            src={c.image}
+                            alt="Avatar Tailwind CSS Component"
+                          />
+                        </div>
+                      </div>
+                    </div>
+                  </td>
+                  <td>{c.name}</td>
+                  <td>{c.instructor}</td>
+                  <td>{c.price}</td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
 };
 
 export default EnrolledClasses;
